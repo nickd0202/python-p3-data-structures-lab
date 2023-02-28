384bc59bfd87f79d9288cfaee1286fc1af404f4b
Nickita Danilovich <nickitadanilovich@gmail.com>
2023-02-27 19:47:17 -0500

    passed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -18,21 +18,48 @@ spicy_foods = [
 
 def get_names(spicy_foods):
     pass
+    list = []
+    for n in spicy_foods:
+        list.append(n.get('name'))
+    return list
 
 def get_spiciest_foods(spicy_foods):
     pass
+    list = [n for n in spicy_foods if n.get('heat_level') > 5]
+    return list
+    
 
 def print_spicy_foods(spicy_foods):
     pass
+    for n in spicy_foods:
+        list = n.get('name') + " (" + n.get('cuisine') + ") " + "| Heat Level: " + "🌶"*n.get('heat_level')
+        print(list)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
+    for n in spicy_foods:
+        if n.get('cuisine') == cuisine:
+            return n
+
 
 def print_spiciest_foods(spicy_foods):
     pass
+    for n in spicy_foods:
+        if n.get("heat_level") > 5:
+            list = n.get('name') + " (" + n.get('cuisine') + ") " + "| Heat Level: " + "🌶"*n.get('heat_level')
+            print(list)
 
 def get_average_heat_level(spicy_foods):
     pass
+    list = []
+    for n in spicy_foods:
+        list.append(n.get('heat_level'))
+    sum_l = sum(list)
+    avg = sum_l/len(list)
+    return avg
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
